7f8266b79aa51cb553aa2495c17e2655f1531eb6
Yernazarovz <yernazarovz@gmail.com>
2018-10-02 18:00:31 +0600

    first commit

---
+++ AIfirst.py
@@ -0,0 +1,38 @@
+A,a,B,b=[ ]
+print("...")
+size_of_matrix_1r=int(input())
+size_of_matrix_1c=int(input())
+print("...")
+size_of_matrix_1r
+size_of_matrix_1c
+if(size_of_matrix_1c!=size_of_matrix2r):
+    print("Error")
+    try:
+        while(len(a)!=size_1c):
+            for i in range (size_1r):
+                a=[int(x) for x in input()split()]
+                if len( )!=size_1c:
+                    print("...")
+                    A.append
+    except IndexError:
+        pass
+if(size_of_matrix_1c!=size_of_matrix2r):
+    print("Error")
+    try:
+        while(len(a)!=size_1c):
+            for i in range (size_1r):
+                a=[int(x) for x in input()split()]
+                if len( )!=size_1c:
+                    print("...")
+                    A.append
+    except IndexError:
+        pass
+
+result=[[0 for x in range(size_2c)] for i in range (1r)]
+for i in range (len(A)):
+    for j in range (len(B[0])):
+        for k in range (len(B)):
+            try:
+                result[i][j]+=A[i][j]*B[k][j]
+            except IndexError:
+                pass
